e4da1eead497696a454a9ebdb44ef003d68ec8fb
CharlizeBrodeth <141261729+CharlizeBrodeth@users.noreply.github.com>
2024-03-07 23:19:33 +0800

    Add files via upload

---
+++ MCO2_new/app.js
@@ -0,0 +1,73 @@
+//Forkast server side 
+
+//Set-up//
+const express = require('express'); // will let us use the module express 
+const server = express(); 
+
+const bodyParser = require('body-parser');
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+
+const handlebars = require('express-handlebars');
+server.set('view engine', 'hbs');
+server.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+server.use(express.static('public'));
+//End of set-up//
+
+//Render Home page (start)//
+server.get('/', function(req, resp){
+    //render the home.html
+    resp.render('start', {
+        layout: 'index',
+        title: 'Welcome to Forkast'
+    });
+});
+
+//Render Login page//
+server.get('/login/', function(req, resp){
+    //render the login.html
+    resp.render('login',{
+        layout: 'index',
+        title: 'Login Page'
+    });
+});
+
+//Render Register page//
+server.get('/register/', function(req, resp){
+    //render the register.html
+    resp.render('register',{
+        layout: 'index',
+        title: 'Register Page'
+    });
+});
+
+//Render Search page//
+server.get('/home/', function(req, resp){
+    //render the search-page.html
+    resp.render('home',{
+        layout: 'index-home',
+        title: 'Forkast Home Page'
+    });
+});
+
+//Render Search Page//
+server.get('/search/', function(req, resp){
+    //render the search-page.html
+    resp.render('search',{
+        layout: 'index-search',
+        title: 'Search Page'
+    });
+});
+
+
+
+
+
+//Port//
+const port = process.env.port || 9090;
+server.listen(port, function(){
+    console.log('listening at port' +port);
+});
